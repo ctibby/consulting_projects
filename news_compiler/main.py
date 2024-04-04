b170558eb2647f4685ee41173df213ee6bb756e8
Chris Tibbitts <tibbitts.chris@gmail.com>
2024-04-03 17:39:09 -0700

    added another website to pull from and updated functions to be more universal with pass through variables

--- news_compiler/main.py
+++ news_compiler/main.py
@@ -7,13 +7,13 @@ model_name = "facebook/bart-large-cnn"
 tokenizer = BartTokenizer.from_pretrained(model_name)
 model = BartForConditionalGeneration.from_pretrained(model_name)
 
-def fetch_article_summary(article_url):
+def fetch_article_summary(article_url, content_class):
     try:
         response = requests.get(article_url)
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        article_content = soup.find('div', class_ = 'entry-content')
-        paragraphs = article_content.find_all('p')[1:]
+        article_content = soup.find('div', class_ = content_class)
+        paragraphs = article_content.find_all('p')
         
         summary = "\n\n".join(paragraph.get_text() for paragraph in paragraphs)
         return summary
@@ -21,50 +21,46 @@ def fetch_article_summary(article_url):
         print(f"An error occurred: {e}")
         return "Summary could not be retrieved."
     
-def summarize_text(text, max_length=150, min_length=50, length_penalty=2.0, num_beams=4):
+def summarize_text(text, max_length=150, min_length=100, length_penalty=2.0, num_beams=4):
     inputs = tokenizer.encode("summarize: " + text, return_tensors="pt", max_length=1024, truncation=True)
     summary_ids = model.generate(inputs, max_length=max_length, min_length=min_length, length_penalty=length_penalty, num_beams=num_beams, early_stopping=True)
     summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
     return summary
 
-def scrape_pal_articles_to_db():
-    url = "https://www.paloaltoonline.com/palo-alto-city/"
+def add_to_db(url, articles_df, articleid, headlineid, urladd, content_class):  
     response = requests.get(url)
-    soup1 = BeautifulSoup(response.content, 'html.parser')
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-    url = "https://www.paloaltoonline.com/category/palo-alto-city/page/2/"
-    response = requests.get(url)
-    soup2 = BeautifulSoup(response.content, 'html.parser')
-
-    url = "https://www.paloaltoonline.com/category/palo-alto-city/page/3/"
-    response = requests.get(url)
-    soup3 = BeautifulSoup(response.content, 'html.parser')
-
-    url = "https://www.mv-voice.com/category/local-news/"
-    response = requests.get(url)
-    soup4 = BeautifulSoup(response.content, 'html.parser')
-
-    article_containers = soup1.find_all('article') + soup2.find_all('article') + soup3.find_all('article') + soup4.find_all('article')
-
-    articles_df = pd.DataFrame(columns=['Headline', 'URL', 'Date and Time', 'Content', 'AISummary'])
+    article_containers = soup.find_all(articleid, limit=1)
 
     for article in article_containers:
-        headline_tag = article.find('h2')
+        headline_tag = article.find(headlineid)
         if headline_tag:
             a_tag = headline_tag.find('a')
             if a_tag and a_tag.has_attr('href'):
                 full_url = a_tag['href']
                 headline = a_tag.get_text(strip=True)
-                date_span = article.find('time', class_='updated')
-                date_time = date_span.get_text(strip=True) if date_span else "Date not found"
+                date_span = article.find('time', class_=['published', 'tnt-date'])
+                date_time = date_span['datetime'] if date_span and 'datetime' in date_span.attrs else "Date not found"
+                print(date_time)
+                full_url = f"{urladd}{full_url}"
                 
-                # Create a temporary DataFrame for the new row
-                temp_df = pd.DataFrame([{'Headline': headline, 'URL': full_url, 'Date and Time': date_time, 'Content': fetch_article_summary(full_url), 'AISummary': summarize_text(fetch_article_summary(full_url))}])
+                article_content = fetch_article_summary(full_url, content_class)
+                temp_df = pd.DataFrame([{'Headline': headline, 'URL': full_url, 'Date and Time': date_time, 'Content': article_content, 'AISummary': summarize_text(article_content)}])
                 articles_df = pd.concat([articles_df, temp_df], ignore_index=True)
                 print(f"{headline} - successfully entered and summarized")
+    return(articles_df)
+
+def scrape_pal_articles_to_db():
+    articles_df = pd.DataFrame(columns=['Headline', 'URL', 'Date and Time', 'Content', 'AISummary'])
+
+    articles_df = add_to_db("https://www.paloaltoonline.com/category/palo-alto-city/", articles_df, 'article', 'h2',"" ,'entry-content')
+    articles_df = add_to_db("https://www.paloaltoonline.com/category/palo-alto-city/page/2/", articles_df, 'article', 'h2',"" ,'entry-content')
+    articles_df = add_to_db("https://www.mv-voice.com/category/local-news/", articles_df, 'article', 'h2',"" ,'entry-content')
+    articles_df = add_to_db("https://www.smdailyjournal.com/news/local/", articles_df, 'article', 'h3', "https://www.smdailyjournal.com/", 'asset-content')
 
     articles_df = articles_df.drop_duplicates(subset='URL')
-    articles_df['Date and Time'] = pd.to_datetime(articles_df['Date and Time'], errors='coerce', format='%B %d, %Y %I:%M %p')
+    articles_df['Date and Time'] = pd.to_datetime(articles_df['Date and Time'], errors='coerce')
     articles_df.sort_values(by='Date and Time', ascending=False, inplace=True)
 
     # Use the most recent date in the filename
@@ -78,7 +74,7 @@ def scrape_pal_articles_to_db():
         for index, row in articles_df.iterrows():
             file.write(f"Headline: {row['Headline']}\nURL: {row['URL']}\nDate and Time: {row['Date and Time'].strftime('%b %d, %Y %I:%M %p') if not pd.isna(row['Date and Time']) else 'Date not found'}\nContent:\n\n\n{row['Content']}\nAISummary:\n\n{row['AISummary']}\n\n---\n\n")
 
-    articles_df.to_csv(f'article_summaries_{most_recent_date}.csv', index=False, encoding='utf-8')
+    # articles_df.to_csv(f'article_summaries_{most_recent_date}.csv', index=False, encoding='utf-8')
 
     print(f"Article summaries have been saved to {filename}.")
 
